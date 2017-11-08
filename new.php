d06fdc04f84c9f9987843db6b8a39e22a2d608ce
xiaoquaml <1084631146@qq.com>
2017-11-08 09:40:27 +0800

    wrote a readme file

---
+++ new.php
@@ -0,0 +1,15 @@
+<?php 
+$arr = array(1,23,43,532,3243,5432);
+function maopao($arr)
+{
+	$count = count($arr);
+	for ( $i = 1 , $i < $count , $i++){
+		for ($j = 0 ,$j < $count-$i , $j++){
+			if($arr[$j]>$arr[$j+1]){
+				$temp = $arr[$j+1];
+				$
+			}
+		}
+	} 
+}
+?>
